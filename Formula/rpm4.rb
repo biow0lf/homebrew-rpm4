99fcac39a4defd3d2abb2a31265a24e65aedb3de
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-31 13:53:19 +0300

    Try

---
+++ Formula/rpm4.rb
@@ -0,0 +1,20 @@
+class Rpm4 < Formula
+  desc "A GNU archiving program"
+  homepage "http://rpm.org"
+  url "http://ftp.rpm.org/releases/rpm-4.13.x/rpm-4.13.0.1.tar.bz2"
+  sha256 "27fc7ba7d419622b1ce34d6507aa70b0808bc344021d298072a0c2ec165f9b0d"
+
+  depends_on "nss"
+  depends_on "nspr"
+  # libarchive
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", '--verbose'
+    system "make", "install"
+  end
+
+  test do
+  end
+end
+
