ea5f7846663cd742d5f36ac362268752300ad486
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-31 16:01:13 +0300

    Update formula

--- Formula/rpm4.rb
+++ Formula/rpm4.rb
@@ -4,13 +4,15 @@ class Rpm4 < Formula
   url "http://ftp.rpm.org/releases/rpm-4.13.x/rpm-4.13.0.1.tar.bz2"
   sha256 "27fc7ba7d419622b1ce34d6507aa70b0808bc344021d298072a0c2ec165f9b0d"
 
-  depends_on "nss"
-  depends_on "nspr"
+  #depends_on "nss"
+  #depends_on "nspr"
   # libarchive
+  
+  # berkeley-db@4
 
   def install
     system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", '--verbose'
+                          "--prefix=#{prefix}", '--verbose', '--with-beecrypt', '--without-archive', '--with-external-db', '--without-lua'
     system "make", "install"
   end
 
